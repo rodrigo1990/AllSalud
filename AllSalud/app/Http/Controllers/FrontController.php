77ad513d47cccd3f1d41242d65200a3b1b8a8483
rodrigo <mcd77.1990@gmail.com>
2019-04-16 17:58:20 -0300

    inyeccion de dependencias en FrontController
    inyeccion de dependencias en FrontController

--- AllSalud/app/Http/Controllers/FrontController.php
+++ AllSalud/app/Http/Controllers/FrontController.php
@@ -3,18 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
+use App\Services\ProvinciaService;
+use App\Services\TipoEstablecimientoService;
+use App\Services\EspecialidadService;
+
 use App\Provincia;
 use App\TipoEstablecimiento;
 use App\Especialidad;
 
 class FrontController extends Controller
 {
+
+    protected $provinciaService;
+    protected $tipoEstablecimientoService;
+    protected $especialidadService;
+
+    function __construct(ProvinciaService $provinciaService,TipoEstablecimientoService $tipoEstablecimientoService,EspecialidadService $especialidadService ){
+
+
+      $this->provinciaService = $provinciaService;
+
+      $this->tipoEstablecimientoService=$tipoEstablecimientoService;
+
+      $this->especialidadService = $especialidadService;
+
+
+    }
+
+
     public function index()
     {	
 
-        $provincias = Provincia::all();
-        $tipos = TipoEstablecimiento::all();
-        $especialidades = Especialidad::orderBy('descripcion', 'desc')->get();
+        $provincias = $this->provinciaService->getProvincias();
+        $tipos =$this->tipoEstablecimientoService->getTipos();
+        $especialidades = $this->especialidadService->getEspecialidades();
 
 
         return view('index',compact('provincias','tipos','especialidades'));
