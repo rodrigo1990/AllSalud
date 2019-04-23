dd80ed4bb1495fb197fc2749cc8be652382c7480
rodrigo <mcd77.1990@gmail.com>
2019-04-23 18:11:18 -0300

    CONVERSION A MAYUSUCULAS JS
    CONVERSION A MAYUSUCULAS JS

--- AllSalud/resources/views/index.blade.php
+++ AllSalud/resources/views/index.blade.php
@@ -369,7 +369,14 @@
 
 
 		function toTitleCase(str) {
-  			return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  			var splitStr = str.toLowerCase().split(' ');
+			   for (var i = 0; i < splitStr.length; i++) {
+			       // You do not need to check if i is larger than splitStr length, as your for does that for you
+			       // Assign it back to the array
+			       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+			   }
+			   // Directly return the joined string
+			   return splitStr.join(' ');
 		}
     </script>
     <script async defer
